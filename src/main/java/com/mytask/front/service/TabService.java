a6fe66b825a504835f73c775cc93129126d4318f
James ABIB <james.bidule@gmail.com>
2023-03-27 21:51:56 +0200

    refacto: add TabService

---
+++ src/main/java/com/mytask/front/service/TabService.java
@@ -0,0 +1,94 @@
+package com.mytask.front.service;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
+
+import java.util.Objects;
+import java.util.Random;
+
+public class TabService {
+    public static HBox createColorTags(Random random) {
+        Color color1 = Color.rgb(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+        Color color2 = Color.rgb(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+        String tooltipStyle = "-fx-font-size: 12px; -fx-font-weight: bold; -fx-text-fill: #ffffff; -fx-background-color: #000000; -fx-padding: 5px;";
+
+        Rectangle colorRect1 = new Rectangle(20, 10, color1);
+        Rectangle colorRect2 = new Rectangle(20, 10, color2);
+
+        for (Color color : new Color[]{color1, color2}) {
+            Rectangle colorRect = color.equals(color1) ? colorRect1 : colorRect2;
+            String labelName = "Couleur: " + color.toString().substring(2, color.toString().length() - 2) + ", Titre: \"" + (random.nextInt(100) + 1) + "\"";
+            Tooltip tooltip = new Tooltip(labelName);
+            tooltip.setStyle(tooltipStyle);
+            Tooltip.install(colorRect, tooltip);
+            tooltip.setStyle(tooltipStyle);
+            colorRect.setArcWidth(10);
+            colorRect.setArcHeight(10);
+            colorRect.setOnMouseEntered(e -> colorRect.setOpacity(0.5));
+            colorRect.setOnMouseExited(e -> colorRect.setOpacity(1));
+            tooltip.setHideDelay(Duration.seconds(0.3));
+            tooltip.setShowDelay(Duration.seconds(0.2));
+        }
+
+        return new HBox(5, colorRect1, colorRect2);
+    }
+
+    public static Label createTitleLabel(Random random) {
+        return new Label("[JAVA] Mise en place de JavaFX " + (random.nextInt(100) + 1));
+    }
+
+    public static HBox createDeadlineBox(Random random) {
+        Image clockImage = new Image(Objects.requireNonNull(TabService.class.getResourceAsStream("/com/mytask/front/icons/horloge.png")));
+        Image clockImageChecked = new Image(Objects.requireNonNull(TabService.class.getResourceAsStream("/com/mytask/front/icons/check.png")));
+
+        ImageView clockImageView = new ImageView(clockImage);
+        clockImageView.setFitHeight(15);
+        clockImageView.setFitWidth(15);
+        Label dueDateLabel = new Label((random.nextInt(30) + 1) + " avr");
+        dueDateLabel.getStyleClass().add("dueDateLabel");
+
+        Button dueDateButton = new Button();
+        dueDateButton.setGraphic(clockImageView);
+        dueDateButton.setStyle("-fx-background-color: transparent; -fx-padding: 0;");
+
+        dueDateButton.setOnAction(e -> {
+            if (clockImageView.getImage() == clockImage) {
+                clockImageView.setImage(clockImageChecked);
+                dueDateLabel.getStyleClass().add("dueDateLabelChecked");
+            } else {
+                clockImageView.setImage(clockImage);
+                dueDateLabel.getStyleClass().remove("dueDateLabelChecked");
+            }
+        });
+
+        return new HBox(5, dueDateButton, dueDateLabel);
+    }
+
+    public static TextField createAssignedToField(Random random) {
+        TextField assignedToField = new TextField("Personne " + (random.nextInt(10) + 1));
+        assignedToField.setEditable(false);
+        assignedToField.getStyleClass().add("dueDateLabel");
+        return assignedToField;
+    }
+
+    public static ImageView createEditImageView() {
+        Image editImage = new Image(Objects.requireNonNull(TabService.class.getResourceAsStream("/com/mytask/front/icons/pencil.png")));
+
+        ImageView editImageView = new ImageView(editImage);
+        editImageView.setFitHeight(15);
+        editImageView.setFitWidth(15);
+        editImageView.setVisible(false); // initially invisible
+
+        return editImageView;
+    }
+
+
+}
