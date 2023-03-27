56244fbad011ffe876d0f9ae2c79acd5e1606bcd
James ABIB <james.bidule@gmail.com>
2023-03-27 09:39:20 +0200

    fix: drag&drop when scrollpane empty

--- src/main/java/com/mytask/front/model/EPage.java
+++ src/main/java/com/mytask/front/model/EPage.java
@@ -3,7 +3,7 @@ package com.mytask.front.model;
 public enum EPage {
 
     SHOW_ALL_TAB("show_all_tab", "All my tabs", 1280, 720),
-    SHOW_TAB("show_tab", "My Tab", 1280, 720),
+    SHOW_TAB("show_tab", "My Tab", 1880, 720),
     INDEX("index", "MyTask", 1280, 720),
     CREATE_TAB("create_tab", "Create Tab", 1280, 720);
 
