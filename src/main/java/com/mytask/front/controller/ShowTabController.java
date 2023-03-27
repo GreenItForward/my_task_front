67b604e64ac9da2ba6aa8c772045a50b8c297e18
James ABIB <james.bidule@gmail.com>
2023-03-27 09:39:20 +0200

    fix: drag&drop when scrollpane empty

--- src/main/java/com/mytask/front/controller/ShowTabController.java
+++ src/main/java/com/mytask/front/controller/ShowTabController.java
@@ -4,15 +4,11 @@ import com.mytask.front.model.EPage;
 import com.mytask.front.service.ScreenService;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.control.Label;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
@@ -24,14 +20,17 @@ public class ShowTabController {
     ScreenService screenService;
 
     @FXML
-    private Label tablesLabel, tableTitleLabel, tableShareCodeLabel, tableDescriptionLabel, todoLabel, inProgressLabel, doneLabel;
+    private Label tablesLabel, todoLabel, inProgressLabel, doneLabel;
     @FXML
     private ListView<?> tablesListView;
     @FXML
     private Button backToMenuBtn, generateInviteCodeBtn, viewMembersBtn;
     
     @FXML
-    private VBox tableInfoVBox, todoColumn, inProgressColumn, doneColumn,  todoTasksList, inProgressTasksList, doneTasksList;
+    private VBox todoTasksList, inProgressTasksList, doneTasksList;
+
+    @FXML
+    private ScrollPane todoTask, inProgressTask, doneTask;
 
     private HBox draggedTask;
 
@@ -49,7 +48,7 @@ public class ShowTabController {
         inProgressLabel.setText("IN PROGRESS");
         doneLabel.setText("DONE");
 
-        // Ajouter des tâches aléatoires
+        // Ajouter des tâches aléatoires (pour les tests avant d'implémenter l'API)
         Random random = new Random();
         for (int i = 0; i < 5; i++) {
             todoTasksList.getChildren().add(createRandomTask(random));
@@ -58,6 +57,7 @@ public class ShowTabController {
         }
 
         // Ajouter des événements de glisser-déposer
+
         setupDragAndDrop();
 
         //TODO: Initialize the controller's logic here
@@ -67,6 +67,7 @@ public class ShowTabController {
     }
 
 
+    // Ajouter des tâches aléatoires (pour les tests avant d'implémenter l'API)
     private HBox createRandomTask(Random random) {
         HBox taskBox = new HBox(10);
         Label titleLabel = new Label("Tâche " + (random.nextInt(100) + 1));
@@ -76,45 +77,25 @@ public class ShowTabController {
         Label dueDateLabel = new Label("Échéance : " + (random.nextInt(30) + 1) + "/" + (random.nextInt(12) + 1) + "/2023");
 
         taskBox.getChildren().addAll(titleLabel, colorRect, assignedToField, dueDateLabel);
+        configureTaskDragAndDrop(taskBox);
+
         return taskBox;
     }
 
     private void setupDragAndDrop() {
-        VBox todoVBox = todoTasksList;
-        VBox inProgressVBox = inProgressTasksList;
-        VBox doneVBox = doneTasksList;
+        ScrollPane[] scrollPanes = new ScrollPane[]{todoTask, inProgressTask, doneTask};
 
-        for (VBox column : new VBox[]{todoVBox, inProgressVBox, doneVBox}) {
-            for (Node taskNode : column.getChildren()) {
-                if (taskNode instanceof HBox) {
-                    HBox taskBox = (HBox) taskNode;
-                    taskBox.setOnDragDetected(event -> {
-                        Dragboard db = taskBox.startDragAndDrop(TransferMode.MOVE);
-                        ClipboardContent content = new ClipboardContent();
-                        content.putString("task");
-                        db.setContent(content);
-                        draggedTask = taskBox;
-                        event.consume();
-                    });
+        for (ScrollPane scrollPane : scrollPanes) {
+            VBox column = (VBox) scrollPane.getContent();
 
-                    taskBox.setOnDragDone(event -> {
-                        if (event.getTransferMode() == TransferMode.MOVE) {
-                            ((VBox) draggedTask.getParent()).getChildren().remove(draggedTask);
-                        }
-                        draggedTask = null;
-                        event.consume();
-                    });
-                }
-            }
-
-            column.setOnDragOver(event -> {
-                if (event.getDragboard().hasString() && "task".equals(event.getDragboard().getString())) {
+            scrollPane.setOnDragOver(event -> {
+                if (event.getGestureSource() != column && event.getDragboard().hasString() && "task".equals(event.getDragboard().getString())) {
                     event.acceptTransferModes(TransferMode.MOVE);
                 }
                 event.consume();
             });
 
-            column.setOnDragDropped(event -> {
+            scrollPane.setOnDragDropped(event -> {
                 if (draggedTask != null && !Objects.equals(draggedTask.getParent(), column)) {
                     column.getChildren().add(draggedTask);
                     event.setDropCompleted(true);
@@ -123,8 +104,36 @@ public class ShowTabController {
                 }
                 event.consume();
             });
+
         }
     }
+
+
+    private void configureTaskDragAndDrop(HBox taskBox) {
+        taskBox.setOnDragDetected(event -> {
+            Dragboard db = taskBox.startDragAndDrop(TransferMode.MOVE);
+            ClipboardContent content = new ClipboardContent();
+            content.putString("task");
+            db.setContent(content);
+            draggedTask = taskBox;
+            event.consume();
+        });
+
+        taskBox.setOnDragDone(event -> {
+            if (event.getTransferMode() == TransferMode.MOVE) {
+                VBox targetParent = (VBox) draggedTask.getParent();
+                targetParent.getChildren().remove(draggedTask);
+                targetParent.getChildren().add(draggedTask);
+            }
+
+            draggedTask = null;
+            event.consume();
+        });
+    }
+
+
+
+
 
 
 }
